2a0699e34aadb76d6787fb8504ccb79dfa71fab9
Serdjei <c.c@bk.ru>
2023-07-03 16:18:45 +0300

    Добавление объекта юсера

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from "./users.service";
+import { IUser } from "../models/IUser";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  constructor(private usersService: UsersService ) {}
+
+  @Get()
+  getUsers(): IUser[] {
+    return this.usersService.getUsers()
+  }
+
+  @Get(':id')
+  getUser( @Param('id') id: string ): IUser {
+    return this.usersService.getUser( +id )
+  }
+
+}
+
